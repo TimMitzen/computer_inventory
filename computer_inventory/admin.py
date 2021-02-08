7ab6b03d7431f3a2a66a99b16347ab900a92849d
timislearning <tim.mitzen@gmail.com>
2021-02-08 11:49:57 -0500

    deploy

--- computer_inventory/admin.py
+++ computer_inventory/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from .models import Computer
 from import_export.admin import ImportExportModelAdmin
 # Register your models here.
-@admin.register(Computer)
 
 class ViewAdmin(ImportExportModelAdmin):
     pass
+
+class ComputerAdmin(admin.ModelAdmin):
+    search_fields = ['computer_name', 'serial_number', 'user_name', 'person_full_name']
+
+admin.site.register(Computer, ComputerAdmin)
+
+
